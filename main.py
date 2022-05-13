8bc19c4f9fb9f1006720cc68a7f67e181e773dd3
duydang2412 <duydang2412@gmail.com>
2022-05-13 19:44:38 +0700

    refactor(main): update test case

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from state import State
 from solver import GreedySolver, AStarSolver
 
 
-goal = State([1, 2, 3, 4, 5, 6, 7, 8, 0])
-initial = State([2, 6, 5, 0, 8, 7, 4, 3, 1])
+goal = State((1, 2, 3, 4, 5, 6, 7, 8, 0))
+initial = State((2, 6, 5, 0, 8, 7, 4, 3, 1))
 print('initial state is', initial.matrix)
 print('goal state is', goal.matrix)
 
-print('greedy misplaced: ', end='')
-GreedySolver(goal).solve(initial, Solver.misplaced)
-print('greedy manhattan: ', end='')
+print('greedy manhattan')
 GreedySolver(goal).solve(initial, Solver.manhattan)
+print('greedy misplaced')
+GreedySolver(goal).solve(initial, Solver.misplaced)
 
-print('A* misplaced: ', end='')
-AStarSolver(goal).solve(initial, Solver.misplaced)
-print('A* manhattan: ', end='')
+print('A* manhattan')
 AStarSolver(goal).solve(initial, Solver.manhattan)
+print('A* misplaced')
+AStarSolver(goal).solve(initial, Solver.misplaced)
